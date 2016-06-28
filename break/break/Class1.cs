6ca892ff9bb34fdec0abe9f206ba869db0379e8a
Ryoki_Okada <karasryo@yahoo.co.jp>
2016-06-28 23:43:54 +0900

    test

---
+++ break/break/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace break{
+    class Class1
+{
+    int unko= 1;
+    int tinko = 2;
+    
+
+
+}
+}
